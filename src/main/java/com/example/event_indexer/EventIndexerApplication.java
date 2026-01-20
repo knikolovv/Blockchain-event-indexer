84bdc5b636b2b999976dced866f97e1ce7fbde2a
Kris <chrissnikolov@gmail.com>
2026-01-20 10:05:05 +0200

    chore: project init

---
+++ src/main/java/com/example/event_indexer/EventIndexerApplication.java
@@ -0,0 +1,13 @@
+package com.example.event_indexer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventIndexerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventIndexerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/event_indexer/EventIndexerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.event_indexer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventIndexerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
